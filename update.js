20682610150c45453710b75a0873f577b3704786
Bugs Bunny <dylanspeedrun@gmail.com>
2021-01-26 14:33:02 +0000

    Testing update

---
+++ update.js
@@ -0,0 +1,27 @@
+const { exec } = require("child_process");
+update();
+
+function update(){
+  exec("git fetch", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+  exec("git checkout origin", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+}
