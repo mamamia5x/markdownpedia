2c02b344cf96df79fcc9c45eb8359adfe9452ca3
Bugs Bunny <dylanspeedrun@gmail.com>
2021-01-26 18:04:44 +0000

    Added pages

--- index.js
+++ index.js
@@ -1,48 +1,29 @@
-var showdown  = require('showdown');
-converter = new showdown.Converter();
-var css = 'https://markdownpedia.tk/server/vital/style.css';
-var fsExtra = require('fs-extra');
-var fs = require('fs');
+const { exec } = require("child_process");
+const fs = require("fs");
 
-var template = fs.readFileSync('vital/template.html') + '';
+update();
 
-fsExtra.emptyDirSync('site/html'); 
-var e404 = fs.readFileSync('vital/404.md') + '';
-var html = converter.makeHtml(e404);
-html = template.replace('{markdown}', html);
-fs.writeFileSync('vital/404.html', html);
-getfiles('site/markdown'); 
-function getfiles(dir){ 
-  var files = fs.readdirSync(dir);
-  var i = 0;
-  while (i < files.length){ 
-    var now = getSecondPart(files[i]);
-    if (now == 'md'){
-      convert(files[i], dir);
-    }
-    else{
-      getfiles(dir + '/' + files[i]);
-    }
-    i++;
+function update(){
+  if (fs.existsSync('markdownpedia')) {
+    command("rm -rf markdownpedia && git clone https://github.com/mamamia5x/markdownpedia.git && node markdownpedia/server.js");
+  }
+  else {
+    command("git clone https://github.com/mamamia5x/markdownpedia.git && node markdownpedia/server.js");
   }
 }
 
-function convert(file, dir){
-  var filename = dir + '/' + file;
-  var content = converter.makeHtml(fs.readFileSync(filename) + '');
-  filename = filename.replace('site/markdown', 'site/html');
-  filename = filename.replace('.md', '.html');
-  var data = template.replace('{markdown}', content);
-  checkIfDir(dir.replace('site/markdown', 'site/html'));
-  fs.writeFileSync(filename, data);
-  console.log('File is ' + file + ' and is in ' + dir);
-}
-function getSecondPart(str) {
-  return str.split('.')[1];
-}
+setInterval(update, 3600000);
 
-function checkIfDir(dir){
-  if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
-  }
+function command (com){
+  exec(com, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
 }
--- site/markdown/github/index.md
+++
@@ -1,4 +0,0 @@
-# GitHub
-[GitHub](https://github.com/) is a popular site used for version control and site hosting. It was started on Feburary 8th, 2008. Making it 12 years old, almost 13.
-
-This article is a stub. Please help edit it at our [GitHub](https://github.com/mamamia5x/markdownpedia/).
