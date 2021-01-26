83ebe92dbe6bc7229a62d34ae7b35e1b48b6a886
Bugs Bunny <dylanspeedrun@gmail.com>
2021-01-26 14:16:43 +0000

    Update site every 10 minutes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const { exec } = require("child_process");
 var path = require('path');
 var app = express();
 var port = 8000;
@@ -13,3 +14,18 @@ app.use(function(req, res, next) {
 app.listen(port);
 console.log(`Now listening on port ${port}`);
 console.log(`Go to http://localhost:${port}`);
+
+function site(){
+  exec("npm run convert", (error, stdout, stderr) => {
+      if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+      }
+      if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+      }
+      console.log(`stdout: ${stdout}`);
+  });
+}
+setInterval(site, 600000);
