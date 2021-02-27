1d2b6b460169cbb3965577349984e9a353300ff5
Reydescel Rodriguez <rherrera@solidemx.com>
2021-02-27 03:30:23 -0700

    Running Prettier --Write to format all code

--- vital/public/search.js
+++ vital/public/search.js
@@ -1,7 +1,7 @@
-var searchpage = "https://markdownpedia.tk/pages/search/";
-searchpage = searchpage.replace("markdownpedia.tk", window.location.hostname);
+var searchpage = 'https://markdownpedia.tk/pages/search/';
+searchpage = searchpage.replace('markdownpedia.tk', window.location.hostname);
 
-function changeSearchUrl(){
-  var search = document.getElementsByClassName("searchbox")[0].value;
-  window.location  = searchpage + '?query=' + search;
+function changeSearchUrl() {
+    var search = document.getElementsByClassName('searchbox')[0].value;
+    window.location = searchpage + '?query=' + search;
 }
